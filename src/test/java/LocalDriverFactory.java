2393cc7578057cf0ee18700d0040c6f70920e07d
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2019-05-24 14:15:29 +0200

    añadidos los drivers

--- src/test/java/LocalDriverFactory.java
+++ src/test/java/LocalDriverFactory.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -6,11 +8,17 @@ class LocalDriverFactory {
     static WebDriver createInstance (String browserName) {
         WebDriver driver = null;
         if (browserName.toLowerCase().contains("firefox")) {
+        	// Descargar Firefox driver (Gecko Driver) de https://github.com/mozilla/geckodriver/releases y copiar en carpeta drivers
+        	System.setProperty("webdriver.gecko.driver", "drivers/geckodriver-v0.24.0-win64/geckodriver.exe");
             driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
             return driver;
         }
         if (browserName.toLowerCase().contains("chrome")) {
+        	// Descargar Chrome driver de https://sites.google.com/a/chromium.org/chromedriver/downloads y copiar en carpeta drivers
+        	System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_win32/chromedriver.exe");
             driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
             return driver;
         }
         return driver;
