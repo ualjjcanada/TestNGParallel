e350252fbb64942adcb4777d0774ee1b526d56c8
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2019-05-24 14:01:15 +0200

    ok solo funciona en chrome

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,5 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -31,5 +33,30 @@ public class FirstTest extends TestBase {
         System.out.println("Google3 Test's Page title is: " + getDriver().getTitle() +" "+ Thread.currentThread().getId());
         Assert.assertEquals(getDriver().getTitle(), "Google");
         System.out.println("Google3 Test Ended! " + Thread.currentThread().getId());
+    }
+    
+    @Test
+    public void testLoginok() throws Exception {
+      getDriver().navigate().to("http://loginapphmis2019jjcanada.azurewebsites.net/");
+      getDriver().findElement(By.linkText("Log in")).click();
+      getDriver().findElement(By.id("Input_Email")).click();
+      getDriver().findElement(By.id("Input_Email")).clear();
+      getDriver().findElement(By.id("Input_Email")).sendKeys("hola2019@ual.es");
+      getDriver().findElement(By.id("Input_Password")).clear();
+      getDriver().findElement(By.id("Input_Password")).sendKeys("ABab12!!");
+      getDriver().findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::button[1]")).click();
+      Assert.assertEquals("Hello hola2019@ual.es!", getDriver().findElement(By.linkText("Hello hola2019@ual.es!")).getText());
+      getDriver().findElement(By.xpath("//form[@id='logoutForm']/ul/li[2]/button")).click();
+      Assert.assertTrue(isElementPresent(By.linkText("Log in")));
+      Assert.assertEquals("Log in", getDriver().findElement(By.linkText("Log in")).getText());
+    }
+
+    private boolean isElementPresent(By by) {
+    	try {
+    		getDriver().findElement(by);
+    		return true;
+    	} catch (NoSuchElementException e) {
+    		return false;
+    	}
     }
 }
